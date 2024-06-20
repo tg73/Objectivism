5c0d376a23527b10b6a7d0c8de6da019f556465d
Tom Glastonbury <t@tg73.net>
2024-06-20 08:47:03 +0100

    Move class to its own file.

---
+++ ObjectivismGH/Parameters/Param_ObjectivismObjectTypeOutput.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Objectivism.Parameters
+{
+    public class Param_ObjectivismObjectTypeOutput : Param_ObjectivismOutput
+    {
+        protected override string OutputType => "Type";
+        protected override string OutputTypePlural => "Types";
+        public override Guid ComponentGuid => new Guid( "55bf273b-08ac-4cd7-a3a2-27bb1228a58c" );
+    }
+}
